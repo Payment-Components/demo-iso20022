957e482f08bb8f544370df619c0a780d3ec5e308
i.maragkopoulos <gm@pc14.eu>
2023-07-27 12:32:13 +0300

    `SEPA EPC DD` added

--- src/main/java/com/paymentcomponents/swift/mx/Main.java
+++ src/main/java/com/paymentcomponents/swift/mx/Main.java
@@ -1,8 +1,8 @@
 package com.paymentcomponents.swift.mx;
 
 import com.paymentcomponents.swift.mx.cbpr.ParseAndValidateCbprMessage;
-import com.paymentcomponents.swift.mx.target2.ParseAndValidateRtgsMessage;
 import com.paymentcomponents.swift.mx.sepa.epc.ct.ParseAndValidateSepaEpcCtMessage;
+import com.paymentcomponents.swift.mx.target2.ParseAndValidateRtgsMessage;
 
 public class Main {
 
